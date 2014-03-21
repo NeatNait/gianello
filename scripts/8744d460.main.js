14e7c423ac937df75e1cea76921bb5e657d850bc
Alvaro Pinot <alvaro@neatnait.com>
2014-03-21 15:29:12 +0100

    Built gianello from commit abb1923 on branch master

---
+++ scripts/8744d460.main.js
@@ -0,0 +1,1 @@
+$(function(){var a=['we <i class="fa fa-heart-o"></i> pizza',"yes we pizza","passion for pizza","be pizza my friend","just pizza it","let's take it nice'n pizza","be pizza","pizza time","let there be pizza","to pizza or not pizza"],b=Math.floor(Math.random()*a.length);$("#slogan").html(a[b]+"<span>!</span>")});
